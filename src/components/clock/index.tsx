a73c18c469c25476f7a558b6cb2fcbddfbee736a
Skinzin <71513905+Skinzin@users.noreply.github.com>
2022-10-04 18:49:33 -0300

    ✨

--- src/components/clock/index.tsx
+++ src/components/clock/index.tsx
@@ -6,19 +6,25 @@ export function Clock() {
     const [progress, setProgress] = useState(0);
 
 
-    useEffect(() => {
-        const tempo = setInterval(() => {
-            setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 2));
-        }, 10000 / 10);
+    // useEffect(() => {
+    //     const tempo = setInterval(() => {
+    //         setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 2));
+    //     }, 10000 / 10);
 
-        return() => {
-            clearInterval(tempo)
-        }
-    }, [])
+    //     return() => {
+    //         clearInterval(tempo)
+    //     }
+    // }, [])
+    // return (
+    //     <>
+    //         {/* <Circular variant="determinate" disableShrink sx={{color: "GrayText", animationDuration: '550ms'}}/> */}
+    //         <Circular variant="determinate" value={progress}/>
+    //     </>
+    // )
+
     return (
         <>
-            {/* <Circular variant="determinate" disableShrink sx={{color: "GrayText", animationDuration: '550ms'}}/> */}
-            <Circular variant="determinate" value={progress}/>
+            <div className="absolute top-0 left-0 w-[100%] h-1 bg-white rounded-tl-lg rounded-tr-lg"></div>
         </>
     )
 }
